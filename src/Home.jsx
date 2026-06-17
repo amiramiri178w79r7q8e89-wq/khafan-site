711e0be32b1b656449f9435c516a5530e54f5e1b
amer <amerea913@email.com>
2026-06-17 04:19:44 +0400

    fix ui home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,27 +2,26 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="home-bg">
+    <div className="home">
 
-      <div className="home-glow"></div>
+      <div className="bg"></div>
 
-      <div className="home-card">
+      <div className="card">
 
-        {/* عکس وسط */}
         <img
-          className="home-img"
           src="https://fi1-ph.ypncdn.com/videos/202505/02/468061265/original/(m=eaSaaTbWx)(mh=ZYcC24OaVFTpvG-v)3.jpg"
-          alt=""
+          className="avatar"
         />
 
         <h1>🎮 ورود به بازی</h1>
-        <p>برای شروع وارد ثبت نام شوید</p>
+        <p>برای شروع ثبت نام کنید</p>
 
         <Link to="/register">
           <button>ثبت نام</button>
         </Link>
 
       </div>
+
     </div>
   );
 }
